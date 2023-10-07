5788251dd87c6079ecfd3e588870d27b8979a054
RAFI <infinityprogrammer01@gmail.com>
2023-10-07 21:05:35 +0400

    fix

--- techno/techno/report/cashflow_simple/cashflow_simple.js
+++ techno/techno/report/cashflow_simple/cashflow_simple.js
@@ -12,26 +12,10 @@ frappe.query_reports["Cashflow Simple"] = {
 			"default": frappe.defaults.get_user_default("Company"),
 			"reqd": 1
 		},
-		// {
-		// 	"fieldname":"bank_account",
-		// 	"label": __("Bank Account"),
-		// 	"fieldtype": "Link",
-		// 	"options": "Account",
-		// 	"reqd": 1,
-		// 	"get_query": function() {
-        //         return {
-        //             "filters": [
-        //                 ["Account", "account_type", "=", "Bank"],
-        //                 ["Account", "company", "=", frappe.query_report.get_filter_value("company")],
-        //             ]
-        //         };
-        //     }
-		// },
 		{
 			"fieldname":"fiscal_year",
 			"label": __("Fiscal Year"),
 			"fieldtype": "Link",
-			"default": erpnext.utils.get_fiscal_year(frappe.datetime.get_today()),
 			"options": "Fiscal Year",
 			"reqd": 1,
 			"width": "60px"
